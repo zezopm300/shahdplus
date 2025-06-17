27b8328533fa56f6f2e4e70849876598ba07fc7c
zezopm300 <z27514821@gmail.com>
2025-06-17 14:50:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -69,9 +69,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // --- 3. Movie Data (Example - Replace with your actual data source) ---
     const moviesData = [
- 
-        {
-            "id": "movie/a-nice-girl-like-you-2020",
+       {
+            "id": 1,
             "title": "A Nice Girl Like You 2020",
             "description": "القصّة : لوسي نيل عازفة كمان، تكتشف إدمان صديقها جيف لمشاهدة المواد الإباحية، فتتشاجر معه، وينفصلا، وتصاب بصدمة عصبية، وتقرر على هذا الاساس تعزيز نفسيها، واكتشاف ذاتها خاصة بعد علاقة الصداقة التي تنشأ بينها وبين جرانت، حيث يساعدها على التغلب على مشاكلها السابقة مع صديقها جيف",
             "poster": "https://i.ibb.co/k2jg6TSd/photo-5852675531542218174-y.jpg",
@@ -84,7 +83,7 @@ document.addEventListener('DOMContentLoaded', () => {
             "duration": "1h 34m"
         },
         {
-            "id": "movie/sleeping-with-the-enemy-1991",
+            "id": 2,
             "title": "Sleeping with the Enemy 1991",
             "description": " تزوجت (لورا) منذ أربع سنوات بالرجل الوسيم (مارتن). يبدو زواجهما مثاليًا في أعين الجميع، ولكن الحقيقة تختلف تمامًا عن هذه الصورة. يعامل مارتن المتسلط لورا بعنف ووحشية ويعتدي عليها، لتصل الزوجة لنقطة تستعد فيها لفعل أي شيء مقابل التخلص من حياتها البائسة. تضع لورا خطة النجاة، والتي تتلخص في قيامها بادعاء الوفاة، وتلفيق كل شيء؛ بحيث تنطلي الخدعة على مارتن. يسير كل شيء حسب الخطة، وتبدأ لورا في العيش بسعادة بهويتها الجديدة، ولكن السعادة لا تدوم طويلًا بعدما تتطور الأحداث بغتة.",
             "poster": "https://i.ibb.co/d4Jmp73r/photo-5852675531542218154-y.jpg",
@@ -98,7 +97,7 @@ document.addEventListener('DOMContentLoaded', () => {
         },
    
         {
-            "id": "movie/moms-friends-2024",
+            "id": 3,
             "title": "Moms Friends 2024",
             "description": " القصّة : فيلم رومانسي جديد حول الرغبات الجنسية والعلاقات الحميمة الساخنة بين الشباب والعلاقات الجنسية التي يمارسونها",
             "poster": "https://i.postimg.cc/dtHdLMNL/photo-5838945848241800438-y.jpg",
@@ -111,7 +110,7 @@ document.addEventListener('DOMContentLoaded', () => {
             "duration": "1h 45m"
         },
         {
-            "id": "movie/blood-pay-2025",
+            "id": 4,
             "title": "Blood Pay 2025",
             "description": "فيلم إثارة خيال علمي تدور أحداثه في الجنة، وهي مدينة خيالية يسيطر فيها الذكاء الاصطناعي على القوى العاملة ويقود العزلة الاجتماعية.",
             "poster": "https://i.ibb.co/v6d90zjN/photo-5789391950099630510-w.jpg",
@@ -124,7 +123,7 @@ document.addEventListener('DOMContentLoaded', () => {
             "duration": "2h 10m"
         },
         {
-            "id": "movie/twisters-2024",
+            "id": 5,
             "title": "Twisters",
             "description": "القصة: مع اشتداد موسم العواصف، تتصادم مسارات مطارد العواصف السابق كيت كوبر ونجم وسائل التواصل الاجتماعي المتهور تايلر أوينز عندما يتم إطلاق العنان لظواهر مرعبة لم يسبق لها مثيل. يجد الزوجان وفرقهما المتنافسة أنفسهم مباشرة في مسارات أنظمة العواصف المتعددة المتقاربة فوق وسط أوكلاهوما في معركة حياتهم.",
             "poster": "https://i.ibb.co/Zp7BnYS3/Untitled.jpg",
