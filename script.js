6a12d2e0855940f065c680ca4daa7b4ad69dafe8
zezopm300 <z27514821@gmail.com>
2025-06-20 14:01:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -245,6 +245,19 @@ document.addEventListener('DOMContentLoaded', () => {
             "rating": "7.9/10",
             "duration": "1h 42m"
         },
+                 {
+            "id": 14,
+            "title":"Snowpiercer (2013)",
+            "description": ": بعد إجراء تجربة فاشلة من أجل التصدي للاحتباس الحراري يحدث عصر جليدي يقضي على الحياة في كوكب الأرض إلا بعض الناجين، ولكنهم يتخذون من قطار ضخم (ثاقب للثلوج) مكانًا للحياة والنجاة. تعيش الصفوة في مقدمة القطار، أما الفقراء فيعيشون في ذيل القطار. لكن بعد مرور 17 عامًا يقوم سكان الذيل بسلسلة جديدة من التمرد بقيادة كيرتس (كريس أيفانز). يُحرر المتمردون سجينًا يُدعى نامجونج (كانج هو سونج)؛ حيث إنه خبير ببوابات القطار، ولذلك يتحد معهم لتبدأ ثورة جديدة",
+            "poster": "https://i.ibb.co/wFWWWYTD/photo-5834902488719935587-w.jpg",
+            "release_date": "2025-6-20",
+            "genre":"Science Fiction",
+            "director": "Bong Joon-ho",
+            "cast":     "Chris Evans as Curtis Everett",
+            "embed_url": "https://vkvideo.ru/video_ext.php?oid=-231089883&id=456239021&hd=2&hash=ba7bc16f7d497779&autoplay=1",
+            "rating": "7.9/10",
+            "duration": "1h 42m"
+        },
 
     ];
 
